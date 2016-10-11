0a125c68347db8108e59d484ed66bb1bee572439
sanlan81 <sanlan81@gmail.com>
2016-10-11 23:23:22 +0300

    added  class RegularExpress for choose  statements change the  characters

---
+++ src/RegularExpress.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpress {
+    public static void main(String[] args) throws IOException {
+        Pattern pattern;
+        Matcher matcher;
+        File file = new File("C:\\Users\\Sasha\\IdeaProjects\\SPD\\homework2\\src\\simpletext.txt");
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        String text = reader.readLine();
+        pattern = Pattern.compile("[A-Z][^?!]+\\.");
+
+        matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println("Step 1: " + matcher.group(0));
+
+            pattern = Pattern.compile("(?i:[aeiouy])");
+            matcher = pattern.matcher(matcher.group(0));
+            System.out.println("Step 2: " + matcher.replaceAll("_"));
+
+        }
+        reader.close();
+    }
+}
